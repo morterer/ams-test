bc02ccf6d2bf54b71c650d9aab5170bb26ebe332
Eric Morter <eric@gibson.net>
2020-08-20 12:34:06 -0500

    Extracting song title from payload

--- ams-test.ino
+++ ams-test.ino
@@ -59,7 +59,7 @@ void connect_callback(uint16_t conn_handle) {
 
   // discover service
   service.discover(conn_handle);
-  if(service.discovered()){
+  if (service.discovered()) {
     Serial.println("Service discovered");
   } else {
     Serial.println("Service not discovered");
@@ -67,21 +67,21 @@ void connect_callback(uint16_t conn_handle) {
 
   // discover characteristic
   characteristic.discover();
-  if(characteristic.discovered()){
+  if (characteristic.discovered()) {
     Serial.println("Characteristic discovered");
   } else {
     Serial.println("Characteristic not discovered");
   }
 
   Bluefruit.requestPairing(conn_handle);
-  if(Bluefruit.connPaired(conn_handle)){
+  if (Bluefruit.connPaired(conn_handle)) {
     Serial.println("Pairing successful");
   } else {
     Serial.println("Pairing failed");
   }
 
   // enable notifications on the characteristic
-  if(characteristic.enableNotify()){
+  if (characteristic.enableNotify()) {
     Serial.println("Notifications enabled");
   } else {
     Serial.println("Notifications NOT enabled");
@@ -91,7 +91,7 @@ void connect_callback(uint16_t conn_handle) {
   // EntityIDTrack TrackAttributeIDTitle
   uint8_t command[] = {2, 2};
   characteristic.write_resp(&command, sizeof(command));
-  
+
 }
 
 /**
@@ -104,28 +104,35 @@ void connect_callback(uint16_t conn_handle) {
 void update_notify_callback(BLEClientCharacteristic* chr, uint8_t* data, uint16_t len) {
   Serial.println("Got a notification");
   Serial.print("Length:"); Serial.println(len);
-/*
-The format of GATT notifications delivered by the MS is shown below:
+  /*
+    The format of GATT notifications delivered by the MS is shown below:
 
-Byte  Value
------------------------
-1     EntityID
-2     AttributeID
-3     EntityUpdateFlags
-4...  Value
+    Byte  Value
+    -----------------------
+    1     EntityID
+    2     AttributeID
+    3     EntityUpdateFlags
+    4...  Value
 
-A GATT notification delivered through the Entity Update characteristic contains the following information:
+    A GATT notification delivered through the Entity Update characteristic contains the following information:
 
-EntityID: The entity to which the subsequent attribute corresponds.
-AttributeID: The attribute whose value is being sent in the notification.
-EntityUpdateFlags: A bitmask whose set bits give the MR specific information about the notification. For example, an MR could be informed that the data had to be truncated in order to fit into the GATT notification.
-Value: A string which corresponds to the value associated with the given attribute.
-*/
+    EntityID: The entity to which the subsequent attribute corresponds.
+    AttributeID: The attribute whose value is being sent in the notification.
+    EntityUpdateFlags: A bitmask whose set bits give the MR specific information about the notification. For example, an MR could be informed that the data had to be truncated in order to fit into the GATT notification.
+    Value: A string which corresponds to the value associated with the given attribute.
+  */
   //  Serial.print("Data:  "); Serial.println(data);
   Serial.print("EntityID:    "); Serial.println(data[0]);
   Serial.print("AttributeID: "); Serial.println(data[1]);
   Serial.print("Truncated:   "); Serial.println(data[2]);
+  memcpy(buffer, data+3, len-3);
+  Serial.println(buffer);
+//  for (int i = 3; i < len; i++) {
+//    Serial.print((char)data[i]);
+//  }
+//  Serial.println();
 
+  
 }
 
 void loop() {
